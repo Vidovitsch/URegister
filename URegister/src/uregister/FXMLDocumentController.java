1daa7612efe9d2347291461c4a446fa524a294fc
Eijkeren,Michiel M.G.B. van <m.vaneijkeren@gmail.com>
2017-05-16 22:59:25 +0200

    Created FXML objects in controller, open right FXML at start

--- URegister/src/uregister/FXMLDocumentController.java
+++ URegister/src/uregister/FXMLDocumentController.java
@@ -10,26 +10,103 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author David
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class FXMLDocumentController implements Initializable
+{
+
     @FXML
-    private Label label;
-    
+    private Button ButtonStartWork;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private Button ButtonFinishWork;
+
+    @FXML
+    private ListView ListViewRegistrations;
+
+    @FXML
+    private Label LabelElapsedTime;
+
+    @FXML
+    private ComboBox ComboBoxYearFilter;
+
+    @FXML
+    private ComboBox ComboBoxMonthFilter;
+
+    @FXML
+    private DatePicker DatePickerDayFilter;
+
+    @FXML
+    private DatePicker DatePickerFromDayFilter;
+
+    @FXML
+    private DatePicker DatePickerTillDayFilter;
+
+    @FXML
+    private TextField TextFieldSalary;
+
+    @FXML
+    private Button ButtonUpdateSalary;
+
+    @FXML
+    private DatePicker DatePickerSelectedRegistrationDate;
+
+    @FXML
+    private TextField TextFieldSelectedRegistrationStartTime;
+
+    @FXML
+    private TextField TextFieldSelectedRegistrationEndTime;
+
+    @FXML
+    private TextArea TextAreaSelectedRegistrationDescription;
+
+    @FXML
+    private Button ButtonUpdateSelectedRegistration;
+
+    @FXML
+    private Button ButtonDeleteSelectedRegistration;
+
+    @FXML
+    private Button ButtonExportCurrentList;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
     }
-    
+
+    @FXML
+    private void handleStartWorkButton(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    private void handleFinishWorkButton(ActionEvent event)
+    {
+
+    }
+
+    public void updateElapsedTime(String timerText)
+    {
+        LabelElapsedTime.setText("Elapsed time: " + timerText);
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         // TODO
-    }    
-    
+    }
+
 }
