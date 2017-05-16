fae11855fe5a776c2e1c1c2e10fc24735f247e7d
Eijkeren,Michiel M.G.B. van <m.vaneijkeren@gmail.com>
2017-05-16 21:37:19 +0200

    FXML

--- URegister/src/main/java/com/ksdgroep/uregister/FXMLController.java
+++ URegister/src/main/java/com/ksdgroep/uregister/FXMLController.java
@@ -1,16 +1,40 @@
 package com.ksdgroep.uregister;
 
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
 
 public class FXMLController implements Initializable {
     
+    
+    @FXML
+    private Pane pane;
+    
     @FXML
     private Label label;
+    
+    @FXML
+    private Button buttonFinishWork;
+            
+    @FXML
+    private Button buttonStartWork;
+    
+    @FXML
+    private ListView listViewRegistrations;
+            @FXML
+    private ComboBox comboBoxYearFilter;
+            @FXML
+    private ComboBox comboBoxMonthFilter;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
