6cbc3b3c1797ba0e26c5b52725a96279ed307f07
Eijkeren,Michiel M.G.B. van <m.vaneijkeren@gmail.com>
2017-05-18 17:09:08 +0200

    Filechooser for excel export implemented

--- URegister/src/uregister/URegister.java
+++ URegister/src/uregister/URegister.java
@@ -11,6 +11,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -18,17 +19,17 @@ import javafx.stage.Stage;
  * @author David
  */
 public class URegister extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
+        FXMLDocumentController c = new FXMLDocumentController();
+        FXMLLoader l = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+        Pane p = (Pane) l.load();
+        c = (FXMLDocumentController) l.getController();
+        Scene scene = new Scene(p);
+        c.setStage(stage);
         stage.setScene(scene);
         stage.show();
-        
-        RegistrationMgr regMgr = new RegistrationMgr();
     }
 
     /**
@@ -37,5 +38,5 @@ public class URegister extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
